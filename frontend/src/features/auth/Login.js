b50867c0d9a5efa3942d2eedf1cd9f69d2224d2c
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-04-25 18:26:01 +1000

    authentication endpoints created

--- frontend/src/features/auth/Login.js
+++ frontend/src/features/auth/Login.js
@@ -1,4 +1,28 @@
+import { useRef, useState, useEffect } from "react";
+import { useNavigate, Link } from "react-router-dom";
+
+import { useDispatch } from "react-redux";
+import { setCredentials } from "./authSlice";
+import { useLoginMutation } from "./authApiSlice";
+
 const Login = () => {
+  const userRef = useRef();
+  const errRef = useRef();
+  const [username, setUsername] = useState("");
+  const [password, setpassword] = useState("");
+  const [errMsg, setErrMsg] = useState("");
+
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const errClass = errMsg ? "errmsg" : "offscreen";
+
+  if (isLoading) {
+    return <p>Loading...</p>;
+  }
+
   return <h1>Login</h1>;
 };
 
