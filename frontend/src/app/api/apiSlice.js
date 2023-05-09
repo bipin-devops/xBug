cc017f2df3d311411165b40e38f1c80087e75602
Bipin Raj Sitoula <viraatsitoula@gmail.com>
2023-05-09 13:48:19 +1000

    baseQuery error fix

--- frontend/src/app/api/apiSlice.js
+++ frontend/src/app/api/apiSlice.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 import { setCredentials } from "../../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "https://xbug-colab.onrender.com",
+  baseUrl: "https://xbug-colab-api.onrender.com",
   credentials: "include",
   prepareHeaders: (headers, { getState }) => {
     const token = getState().auth.token;
